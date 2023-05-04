14c83cd868a5e1789189acaf06590e7acddcec79
David Barrero <davbarfer@gmail.com>
2023-05-04 15:40:39 +0200

    Restructure Folder

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,5 +4,5 @@
 
 <script lang="ts" setup>
 // Este archivo de vue llama a los distntos componentes necesarios que tengan que ver con el home
-  import HelloWorld from '@/components/HelloWorld.vue'
+  import HelloWorld from '@/components/home/HelloWorld.vue'
 </script>
