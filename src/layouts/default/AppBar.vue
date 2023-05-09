c172b68e2976f20aa82f70434560bd2b6238c056
David Barrero <davbarfer@gmail.com>
2023-05-09 14:36:14 +0200

    ADD: Mobile basic navegation

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -6,14 +6,23 @@
     <v-app-bar-title>
       ESTIEM LG SEVILLE
     </v-app-bar-title>
+    <!-- Use the v-tabs in desktop mode -->
     <v-tabs>
       <v-tab :to="{name:'Home'}">Home</v-tab>
       <v-tab :to="{name:'Events'}">Events</v-tab>
     </v-tabs>
+    <!-- Use navegation drawer for mobile -->
+    <v-navigation-drawer
+      v-model="drawer"
+      location="left"
+      temporary
+    >
+    </v-navigation-drawer>
   </v-app-bar>
 </template>
 
 <script lang="ts" setup>
+  let drawer=false
   // Esta archivo de vue contiene el menu que siempre se muestra
 </script>
 <style>
