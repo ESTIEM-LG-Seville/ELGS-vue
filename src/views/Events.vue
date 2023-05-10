a2d2a5aacfae7a3e03423b15fb43449b61bf74ba
David Barrero <davbarfer@gmail.com>
2023-05-10 18:37:38 +0200

    Menu for events

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -1,10 +1,29 @@
 <template>
-    <p>HELLO WORLD</p>
-    <v-tabs>
-
+    <v-tabs
+        v-model="tab"
+        align-tabs="center"
+    >
+        <v-tab :value="LGJpage">Let's Get a Job</v-tab>
+        <v-tab :value="TIMESpage">TIMES</v-tab>
     </v-tabs>
+    <v-window v-model="tab">
+        <v-window-item :value="LGJpage">
+            <LGJ></LGJ>
+        </v-window-item>
+        <v-window-item :value="TIMESpage">
+            <TIMES></TIMES>
+        </v-window-item>
+    </v-window>
 </template>
 
-<script>
+<script lang="ts" setup>
 // Este archivo llama a los componentes necesarios para desplegar los distintos componentes
+import { ref } from "vue";
+import LGJ from '../components/events/LGJ.vue'
+import TIMES from '../components/events/TIMES.vue'
+
+const tab = ref(null)
 </script>
+
+<style>
+</style>
