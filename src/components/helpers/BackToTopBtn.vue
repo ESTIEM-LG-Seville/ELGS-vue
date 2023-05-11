fb267a1aafbec38186c98f7bb77d5ee8dbaac1bb
David Barrero <davbarfer@gmail.com>
2023-05-11 18:46:57 +0200

    Fix: btn show when scroll

--- src/components/helpers/BackToTopBtn.vue
+++ src/components/helpers/BackToTopBtn.vue
@@ -10,24 +10,19 @@
         color="primary"
         @click="toTop"
     >
-        <v-icon>keyboard_arrow_up</v-icon>
+        <v-icon icon="fas fa-arrow-up" />
     </v-btn>
 </template>
 
-<script>
-export default {
-    data: () => ({
-        fab: false
-    }),
-    methods: {
-        onScroll (e) {
+<script lang="ts" setup>
+import { ref } from 'vue'
+const fab = ref(false);
+        function onScroll (e:any) {
             if (typeof window === 'undefined') return
-            const top = window.pageYOffset ||   e.target.scrollTop || 0
-            this.fab = top > 20
-        },
-        toTop () {
+                const top = window.pageYOffset ||   e.target.scrollTop || 0
+                fab.value = top > 20
+        };
+        function toTop () {
             this.$vuetify.goTo(0)
-        }
-    }
-}
+        };
 </script>
