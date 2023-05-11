d8688c3cebe3e1c95cb05aec7e1d0b96e8322d67
David Barrero <davbarfer@gmail.com>
2023-05-11 12:18:48 +0200

    ADD: Set up icons

--- src/main.ts
+++ src/main.ts
@@ -12,9 +12,20 @@ import { createApp } from 'vue'
 
 // Plugins
 import { registerPlugins } from '@/plugins'
-
+// Fontawesome
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+/* import specific icons */
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+/* add icons to the library */
+library.add(fas, fab, far)
 const app = createApp(App)
 
 registerPlugins(app)
 
+app.component('font-awesome-icon', FontAwesomeIcon);
 app.mount('#app')
