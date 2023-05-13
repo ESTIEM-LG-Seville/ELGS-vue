111a28909de7568af61ad59402fc69f68c7c5d7f
David Barrero <davbarfer@gmail.com>
2023-05-13 20:16:19 +0200

    Define Props

--- src/components/events/LGJ.vue
+++ src/components/events/LGJ.vue
@@ -1,8 +1,11 @@
 <template>
-    <h1>{{ $attrs.event.name }}</h1>
-    <p>{{ $attrs.event.description }}</p>
+    <h1>{{ props.event.name }}</h1>
+    <p>{{ props.event.description }}</p>
 </template>
 
 <script lang="ts" setup>
 // Este archivo contendra lo necesario acerca del Let's get a job
+const props = defineProps({
+    event: Object
+})
 </script>
