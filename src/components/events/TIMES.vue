fab294964a70028963509192726c22595d2ed2bc
David Barrero <davbarfer@gmail.com>
2023-05-16 22:30:08 +0200

    ADD: TIMES, timeline, Local

--- src/components/events/TIMES.vue
+++ src/components/events/TIMES.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-container>
+    <v-container align="center">
         <v-card 
             max-width="90%"
             variant="outlined"
@@ -7,13 +7,22 @@
             <v-card-title>{{ props.event.name }}</v-card-title>
             <v-card-text>{{ props.event.description }}</v-card-text>
         </v-card>
-        <timelinetimes></timelinetimes>
+        <timelinetimes :fases="FasesData"></timelinetimes>
+        <v-card
+            max-width="90%"
+            variant="outlined"
+        >
+            <v-card-title>{{ props.event.name + '\xa0' + 'Local Sevilla' }}</v-card-title>
+            <v-card-text>{{ props.event.detail1 }}</v-card-text>
+        </v-card>
     </v-container>
 </template>
 
 <script lang="ts" setup>
 // Este archivo contendra lo necesario acerca del TIMES
 import timelinetimes from "./timeLineTIMES.vue"
+import { FasesTIMES } from "../helpers/eventsInfo.json"
+import { readonly } from "vue"
 interface Event {
     name: string
     description: string
@@ -23,4 +32,11 @@ interface Event {
 const props = defineProps<{
     event: Event
 }>()
+const FasesData = readonly(FasesTIMES)
 </script>
+
+<style scoped>
+.v-card {
+    margin: 2%;
+}
+</style>
--- src/components/events/TimeLineTIMES.vue
+++ src/components/events/TimeLineTIMES.vue
@@ -2,34 +2,40 @@
     <v-timeline direction="horizontal">
         <v-timeline-item>
             <template v-slot:opposite>
-                Opposite content
+                <div class="font-italic">{{ props.fases.local }}</div>
             </template>
-            <div>
-                <div class="text-h6">Content title</div>
-            </div>
+                <div class="text-h5">Fase Local</div>
         </v-timeline-item>
 
         <v-timeline-item>
           <template v-slot:opposite>
-            Opposite content
+            <div class="font-italic">{{ props.fases.semifinal }}</div>
           </template>
           <div>
-            <div class="text-h6">Content title</div>
+            <div class="text-h5">Semifinal</div>
           </div>
         </v-timeline-item>
 
         <v-timeline-item>
           <template v-slot:opposite>
-            Opposite content
+            <div class="font-italic">{{ props.fases.final }}</div>
           </template>
           <div>
-            <div class="text-h6">Content title</div>
+            <div class="text-h5">Final</div>
           </div>
         </v-timeline-item>
     </v-timeline>
 </template>
 
 <script lang="ts" setup>
+interface Fases {
+    local: string
+    semifinal: string
+    final: string
+}
+const props = defineProps<{
+    fases: Fases
+}>()
 </script>
 
 <style scoped>
