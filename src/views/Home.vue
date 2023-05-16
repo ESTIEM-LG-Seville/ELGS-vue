ea187e846c0f9d84d9bb1142a05d299b25f070b4
BuildTools <unconfigured@null.spigotmc.org>
2023-05-16 14:58:21 +0200

    Prueba

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,50 @@
 <template>
-  <HelloWorld />
+  <v-carousel
+    cycle
+    height="750"
+    hide-delimiter-background
+    show-arrows="hover"
+  >
+    <v-carousel-item
+      v-for="(slide, i) in slides"
+      :key="i"
+    >
+      <v-sheet
+        :color="images[i]"
+        height="100%"
+      >
+        <div class="d-flex fill-height justify-center align-center">
+          <div class="text-h2">
+            {{ slide }} Slide
+          </div>
+        </div>
+      </v-sheet>
+    </v-carousel-item>
+  </v-carousel>
+
+
+
 </template>
 
-<script lang="ts" setup>
-// Este archivo de vue llama a los distntos componentes necesarios que tengan que ver con el home
-  import HelloWorld from '@/components/home/HelloWorld.vue'
+<script>
+  export default {
+    data () {
+      return {
+        images: [
+          'indigo',
+          'warning',
+          'pink darken-2',
+          'red lighten-1',
+          'deep-purple accent-4',
+        ],
+        slides: [
+          'First',
+          'Second',
+          'Third',
+          'Fourth',
+          'Fifth',
+        ],
+      }
+    },
+  }
 </script>
