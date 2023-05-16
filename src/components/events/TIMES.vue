1a105032cf48bd8ef9d153b267332e06e8baa72f
David Barrero <davbarfer@gmail.com>
2023-05-16 14:57:44 +0200

    CHANGE: times content

--- src/components/events/TIMES.vue
+++ src/components/events/TIMES.vue
@@ -1,7 +1,26 @@
 <template>
-    <p>HELLO WORLD TIMES</p>
+    <v-container>
+        <v-card 
+            max-width="90%"
+            variant="outlined"
+        >
+            <v-card-title>{{ props.event.name }}</v-card-title>
+            <v-card-text>{{ props.event.description }}</v-card-text>
+        </v-card>
+        <timelinetimes></timelinetimes>
+    </v-container>
 </template>
 
 <script lang="ts" setup>
 // Este archivo contendra lo necesario acerca del TIMES
+import timelinetimes from "./timeLineTIMES.vue"
+interface Event {
+    name: string
+    description: string
+    detail1: string
+    detail2: string
+}
+const props = defineProps<{
+    event: Event
+}>()
 </script>
---
+++ src/components/events/TimeLineTIMES.vue
@@ -0,0 +1,39 @@
+<template>
+    <v-timeline direction="horizontal">
+        <v-timeline-item>
+            <template v-slot:opposite>
+                Opposite content
+            </template>
+            <div>
+                <div class="text-h6">Content title</div>
+            </div>
+        </v-timeline-item>
+
+        <v-timeline-item>
+          <template v-slot:opposite>
+            Opposite content
+          </template>
+          <div>
+            <div class="text-h6">Content title</div>
+          </div>
+        </v-timeline-item>
+
+        <v-timeline-item>
+          <template v-slot:opposite>
+            Opposite content
+          </template>
+          <div>
+            <div class="text-h6">Content title</div>
+          </div>
+        </v-timeline-item>
+    </v-timeline>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+.v-card {
+    margin: 2%;
+}
+</style>
