6a8da9f0b20d3c320297da9a6ebaa88edde151e3
David Barrero <davbarfer@gmail.com>
2023-06-28 17:09:12 +0200

    Create: Slogan component

---
+++ src/components/home/Slogan.vue
@@ -0,0 +1,35 @@
+<template>
+<v-card
+    align="center"
+>
+    <v-card-title>
+        WORK HARD PLAY HARD
+    </v-card-title>
+    <v-card-actions
+    >
+        <v-btn
+        icon="fas fa-chevron-down"
+        @click="show = !show"
+        >
+        </v-btn>
+    </v-card-actions>
+    <v-expand-transition>
+      <div v-show="show">
+        
+      </div>
+    </v-expand-transition>
+</v-card>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+let show = ref(false);
+</script>
+
+<style scoped>
+.v-card-actions {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,10 @@
 <template>
   <HelloWorld />
+  <Slogan />
 </template>
 
 <script lang="ts" setup>
 // Este archivo de vue llama a los distntos componentes necesarios que tengan que ver con el home
   import HelloWorld from '@/components/home/HelloWorld.vue'
+  import Slogan from '@/components/home/Slogan.vue'
 </script>
