6e2ff91e38199260e426469a718e3fa168b83bbb
David Barrero <davbarfer@gmail.com>
2023-06-28 17:51:26 +0200

    ADD: Mission_Vision

--- src/components/home/Slogan.vue
+++ src/components/home/Slogan.vue
@@ -14,15 +14,16 @@
         </v-btn>
     </v-card-actions>
     <v-expand-transition>
-      <div v-show="show">
-        
-      </div>
+        <mission-vision v-show="show" :data="VM">
+        </mission-vision>
     </v-expand-transition>
 </v-card>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
+import MissionVision from '@/components/home/MissionVision.vue';
+import VM from "@/components/helpers/VM.json"
 let show = ref(false);
 </script>
 
