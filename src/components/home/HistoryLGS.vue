ba0a53ece612d1e3d4446231fb13e5c182f143e2
David Barrero <davbarfer@gmail.com>
2023-07-27 22:27:21 +0200

    Create History component

---
+++ src/components/home/HistoryLGS.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+
+<script setup lang="ts">
+interface Data {
+    OurHistory: Array<{
+        year: number,
+        description: string
+    }>
+}
+const props = defineProps<{
+    data: Data
+}>()
+</script>
+
+<style>
+</style>
