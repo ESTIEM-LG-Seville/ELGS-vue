13d48e1d9a91bd66477c5420cea2642945833e4d
David Barrero <davbarfer@gmail.com>
2023-07-30 17:34:54 +0200

    Create Slider Component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,50 +1,7 @@
 <template>
-  <v-carousel
-    cycle
-    height="750"
-    hide-delimiter-background
-    show-arrows="hover"
-  >
-    <v-carousel-item
-      v-for="(slide, i) in slides"
-      :key="i"
-    >
-      <v-sheet
-        :color="images[i]"
-        height="100%"
-      >
-        <div class="d-flex fill-height justify-center align-center">
-          <div class="text-h2">
-            {{ slide }} Slide
-          </div>
-        </div>
-      </v-sheet>
-    </v-carousel-item>
-  </v-carousel>
-
-
-
+<SliderHome />
 </template>
 
-<script>
-  export default {
-    data () {
-      return {
-        images: [
-          'indigo',
-          'warning',
-          'pink darken-2',
-          'red lighten-1',
-          'deep-purple accent-4',
-        ],
-        slides: [
-          'First',
-          'Second',
-          'Third',
-          'Fourth',
-          'Fifth',
-        ],
-      }
-    },
-  }
+<script setup lang="ts">
+import SliderHome from '@/components/home/SliderHome.vue'
 </script>
