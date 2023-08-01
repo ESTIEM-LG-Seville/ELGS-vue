0ca599f5c0641d006e7947bea8b2ba421d0eb724
David Barrero <davbarfer@gmail.com>
2023-08-01 19:15:45 +0200

    Adjust carousel height

--- src/components/home/SliderHome.vue
+++ src/components/home/SliderHome.vue
@@ -1,7 +1,7 @@
 <template>
   <v-carousel
     cycle
-    height="750"
+    :height="carouselHeight"
     hide-delimiter-background
     show-arrows="hover"
   >
@@ -16,6 +16,7 @@
 </template>
 
 <script setup lang="ts">
+import { ref, onMounted, onUnmounted } from "vue";
 import CM_01 from "@/assets/Slide/CM_01.jpg";
 import TIMES_01 from "@/assets/Slide/TIMES_01.png";
 import TIMES_02 from "@/assets/Slide/TIMES_02.png";
@@ -24,4 +25,19 @@ const images = [CM_01, TIMES_01, TIMES_02];
 const info = ["LXIII Council Meeting", "TIMES Final 2023 Photo", "TIMES Final 2023 Winner"];
 
 const getImageUrl = (image: any) => image;
+
+const carouselHeight = ref(window.innerHeight * 0.7);
+
+onMounted(() => {
+  const updateCarouselHeight = () => {
+    carouselHeight.value = window.innerHeight * 0.7;
+  };
+
+  window.addEventListener("resize", updateCarouselHeight);
+
+  // Limpia el event listener cuando el componente se desmonte
+  onUnmounted(() => {
+    window.removeEventListener("resize", updateCarouselHeight);
+  });
+});
 </script>
