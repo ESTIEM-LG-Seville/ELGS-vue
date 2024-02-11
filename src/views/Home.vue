dbf47da16fcad92ade4eb89c7af9aeb636a40112
David Barrero <davbarfer@gmail.com>
2024-02-11 20:00:23 +0100

    Commented History

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,13 @@
 <template>
-  <!-- <SliderHome /> -->
-  <HistoryLGS class="pt-6" :data="OurHistory"/>
+  <SliderHome />
   <Slogan/>
+  <!-- <HistoryLGS class="pt-6" :data="OurHistory"/> -->
 </template>
 
 <script setup lang="ts">
 // Este archivo de vue llama a los distntos componentes necesarios que tengan que ver con el home
-// import SliderHome from '@/components/home/SliderHome.vue'
+import SliderHome from '@/components/home/SliderHome.vue'
 import Slogan from '@/components/home/Slogan.vue'
-import HistoryLGS from '@/components/home/HistoryLGS.vue';
-import OurHistory from '@/components/helpers/history.json'
+// import HistoryLGS from '@/components/home/HistoryLGS.vue';
+// import OurHistory from '@/components/helpers/history.json'
 </script>
