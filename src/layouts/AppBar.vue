b830b739c39fc7b1134d29e8e49c500e4c7e90d1
David Barrero <davbarfer@gmail.com>
2024-03-08 20:01:29 +0100

    Change title to ESTIEM logo

--- src/layouts/AppBar.vue
+++ src/layouts/AppBar.vue
@@ -4,8 +4,8 @@
     class="elevation-0"
   >
     <v-app-bar-nav-icon @click="drawer = !drawer" icon="fas fa-bars" ></v-app-bar-nav-icon>
-    <v-app-bar-title>
-      ESTIEM LG SEVILLE
+    <v-app-bar-title class="d-flex align-center">
+      <EstiemLgSevilleAppbar class="appbarlogo"/>
     </v-app-bar-title>
     <!-- Use the v-tabs in desktop mode -->
     <v-tabs v-model="tab">
@@ -31,6 +31,7 @@
 import { ref, watch, onMounted, onUnmounted } from 'vue';
 import { useRoute } from 'vue-router';
 import FooterDrawer from '@/components/footers/FooterDrawer.vue';
+import EstiemLgSevilleAppbar from '@/assets/EstiemLgSevilleAppbar.vue';
 
 const drawer = ref(false);
 const tab = ref<string | null>(null); // Ahora permitimos que tab pueda ser de tipo 'string' o 'null'
@@ -62,6 +63,9 @@ const stopWatchingRoute = watch(
 );
 </script>
 <style scoped>
+.appbarlogo {
+  height: 32px;
+}
 @media screen and (max-width: 1280px) {
   .v-slide-group {
     display: none !important;
